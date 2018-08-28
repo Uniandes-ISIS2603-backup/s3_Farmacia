3820efb6ea43d13c78a0ddb17f9569837ece6e56
estudiante <estudiante@ISIS2603-316.sis.virtual.uniandes.edu.co>
2018-08-28 17:08:35 -0500

    facturas

---
+++ s3_farmacia-back/src/main/java/co/edu/uniandes/csw/farmacia/entities/FacturaEntity.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.farmacia.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author estudiante
+ */
+@Entity
+public class FacturaEntity extends BaseEntity implements Serializable {
+    
+        public enum TipoFactura{
+        REAPROVISIONAMIENTO, DESPACHO     
+    }
+    private Long id;
+    
+    private String Fecha;
+    
+    private Double precio;
+    
+    private String productos;
+    
+    private Integer unidades;
+    
+    private TipoFactura tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public String getProductos() {
+        return productos;
+    }
+
+    public Integer getUnidades() {
+        return unidades;
+    }
+
+    public TipoFactura getTipo() {
+        return tipo;
+    }
+    
+    
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFecha(String Fecha) {
+        this.Fecha = Fecha;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public void setProductos(String productos) {
+        this.productos = productos;
+    }
+
+    public void setUnidades(Integer unidades) {
+        this.unidades = unidades;
+    }
+
+    public void setTipo(TipoFactura tipo) {
+        this.tipo = tipo;
+    }
+    
+ 
+    
+    
+}
